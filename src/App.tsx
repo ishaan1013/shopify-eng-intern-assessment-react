bd6f7b8b84438465b86d3a8484b9026d58ee916f
Ishaan Dey <ishaan1013@gmail.com>
2024-01-17 23:13:40 -0500

    Add app logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,42 @@
 import React, { useEffect, useState } from "react"
 
 export default function App() {
+  const [time, setTime] = useState(0)
+  const [counting, setCounting] = useState(false)
+
+  const [laps, setLaps] = useState([])
+  const [lastLap, setLastLap] = useState(0)
+
+  // const timeUnits = getUnits(time)
+
+  useEffect(() => {
+    let interval: NodeJS.Timer
+    if (counting) {
+      interval = setInterval(() => setTime((prev) => prev + 1), 10)
+    }
+    return () => clearInterval(interval)
+  }, [counting, time])
+
+  const reset = () => {
+    setTime(0)
+    setLaps([])
+    setLastLap(0)
+    setCounting(false)
+  }
+
+  const lap = () => {
+    setLaps((prev) => [time - lastLap, ...prev])
+    setLastLap(time)
+  }
+
+  const stop = () => {
+    setCounting(false)
+  }
+
+  const start = () => {
+    setCounting(true)
+  }
+
   return (
     <main>
       <div className="container"></div>
