4c6f8a87545b54594997fd83d10f76dbceb53f91
Ishaan Dey <ishaan1013@gmail.com>
2024-01-17 23:15:32 -0500

    Add stopwatch buttons

--- src/StopWatchButton.tsx
+++ src/StopWatchButton.tsx
@@ -1,7 +1,42 @@
-import React from 'react'
+import React from "react"
 
-export default function StopWatchButton() {
-    return(
-        <div></div>
-    )
+export default function StopWatchButton({
+  variant,
+  onClick,
+  disabled,
+}: {
+  variant: "start" | "stop" | "lap" | "reset"
+  onClick: () => void
+  disabled?: boolean
+}) {
+  switch (variant) {
+    case "start":
+      return (
+        <button onClick={onClick} className="button-green">
+          Start
+        </button>
+      )
+    case "stop":
+      return (
+        <button onClick={onClick} className="button-red">
+          Stop
+        </button>
+      )
+    case "lap":
+      return (
+        <button className="button-standard" onClick={onClick}>
+          Lap
+        </button>
+      )
+    case "reset":
+      return (
+        <button
+          className="button-standard"
+          disabled={disabled}
+          onClick={onClick}
+        >
+          Reset
+        </button>
+      )
+  }
 }
