a8a21ad0bedff6584273544a3a61c30fd32ba531
Ishaan Dey <ishaan1013@gmail.com>
2024-01-17 23:14:51 -0500

    Create primary stopwatch display + time units helper

--- src/StopWatch.tsx
+++ src/StopWatch.tsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import React from "react"
 
-export default function StopWatch() {
-    return(
-        <div></div>
-    )
+export default function StopWatch({
+  m,
+  s,
+  ms,
+}: {
+  m: number
+  s: number
+  ms: number
+}) {
+  return (
+    <div className="stopwatch-time mono">
+      {m.toString().padStart(2, "0")}:{s.toString().padStart(2, "0")}.
+      {ms.toString().padStart(2, "0")}
+    </div>
+  )
+}
+
+export function getUnits(time: number) {
+  const m = Math.floor((time % 360000) / 6000)
+  const s = Math.floor((time % 6000) / 100)
+  const ms = time % 100
+  return { m, s, ms }
 }
